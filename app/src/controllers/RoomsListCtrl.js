da72e03d188ffd52b76fde4ea754f85d39e8d81c
Rich Greenhill <rgnet1@yahoo.com>
2016-01-09 12:17:45 -0800

    additional

---
+++ app/src/controllers/RoomsListCtrl.js
@@ -0,0 +1,51 @@
+(function(){
+  function RoomsListCtrl($scope, $uibModal, $location, $cookies, RoomFactory){
+    var roomsListCtrl = this;
+    
+    this.allRooms = RoomFactory.all;
+    
+    this.createRoom = function(room){
+      RoomFactory.createRoom(room);
+    };
+    
+    this.deleteRoom = function(room){
+      RoomFactory.deleteRoom(room);
+    };
+    
+    this.openModal = function (size) {
+      console.log('clicked');
+
+      let modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'templates/modals/roomModal.html',
+        controller: 'RoomModalCtrl as roomModal',
+        size: size
+      });
+
+      modalInstance.result.then(function (roomName) {
+        roomsListCtrl.createRoom(roomName);
+      });
+      
+    };
+    
+    this.changeUsername = function(){
+      let modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'templates/modals/usernameModal.html',
+        controller: 'UsernameModalCtrl as modal',
+        backdrop: 'static'
+      });
+    
+      modalInstance.result.then(function (username) {
+        $cookies.put('currentUser', username);
+      });
+    }
+    
+  }
+  
+  angular
+    .module('dialogg')
+    .controller('RoomsListCtrl', ['$scope', '$uibModal', '$location', '$cookies', 'RoomFactory', RoomsListCtrl]);
+
+}());
+
